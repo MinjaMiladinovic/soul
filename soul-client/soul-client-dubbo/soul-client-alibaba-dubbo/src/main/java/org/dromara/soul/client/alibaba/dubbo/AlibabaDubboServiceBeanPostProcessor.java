748fbb140c3e8b1a5e7ddf6aabb05f9a31d0d0b5
Minja Miladinović <69137994+MinjaMiladinovic@users.noreply.github.com>
2020-12-09 13:38:15 +0100

    Update AlibabaDubboServiceBeanPostProcessor.java

--- soul-client/soul-client-dubbo/soul-client-alibaba-dubbo/src/main/java/org/dromara/soul/client/alibaba/dubbo/AlibabaDubboServiceBeanPostProcessor.java
+++ soul-client/soul-client-dubbo/soul-client-alibaba-dubbo/src/main/java/org/dromara/soul/client/alibaba/dubbo/AlibabaDubboServiceBeanPostProcessor.java
@@ -15,145 +15,125 @@
  * limitations under the License.
  */
 
-package org.dromara.soul.client.alibaba.dubbo;
-
-import com.alibaba.dubbo.common.Constants;
-import com.alibaba.dubbo.common.utils.StringUtils;
-import com.alibaba.dubbo.config.spring.ServiceBean;
+package org.dromara.soul.client.springcloud.init;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
 import lombok.extern.slf4j.Slf4j;
 import org.dromara.soul.client.common.utils.OkHttpTools;
-import org.dromara.soul.client.dubbo.common.annotation.SoulDubboClient;
-import org.dromara.soul.client.dubbo.common.config.DubboConfig;
-import org.dromara.soul.client.dubbo.common.dto.MetaDataDTO;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.util.ClassUtils;
+import org.dromara.soul.client.springcloud.annotation.SoulSpringCloudClient;
+import org.dromara.soul.client.springcloud.config.SoulSpringCloudConfig;
+import org.dromara.soul.client.springcloud.dto.SpringCloudRegisterDTO;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * The Alibaba Dubbo ServiceBean PostProcessor.
+ * The type Soul client bean post processor.
  *
- * @author xiaoyu
+ * @author xiaoyu(Myth)
  */
 @Slf4j
-public class AlibabaDubboServiceBeanPostProcessor implements ApplicationListener<ContextRefreshedEvent> {
+public class SpringCloudClientBeanPostProcessor implements BeanPostProcessor {
 
-    private DubboConfig dubboConfig;
-
-    private ExecutorService executorService;
+    private final ThreadPoolExecutor executorService;
 
     private final String url;
 
-    public AlibabaDubboServiceBeanPostProcessor(final DubboConfig dubboConfig) {
-        String contextPath = dubboConfig.getContextPath();
-        String adminUrl = dubboConfig.getAdminUrl();
+    private final SoulSpringCloudConfig config;
+
+    private final Environment env;
+
+    /**
+     * Instantiates a new Soul client bean post processor.
+     *
+     * @param config the soul spring cloud config
+     * @param env    the env
+     */
+    public SpringCloudClientBeanPostProcessor(final SoulSpringCloudConfig config, final Environment env) {
+        String contextPath = config.getContextPath();
+        String adminUrl = config.getAdminUrl();
+        String appName = env.getProperty("spring.application.name");
         if (contextPath == null || "".equals(contextPath)
-                || adminUrl == null || "".equals(adminUrl)) {
-            throw new RuntimeException("Alibaba dubbo client must config the contextPath, adminUrl");
+                || adminUrl == null || "".equals(adminUrl)
+                || appName == null || "".equals(appName)) {
+            throw new RuntimeException("spring cloud param must config the contextPath, adminUrl and appName");
         }
-        this.dubboConfig = dubboConfig;
-        url = dubboConfig.getAdminUrl() + "/soul-client/dubbo-register";
+        this.config = config;
+        this.env = env;
+        this.url = adminUrl + "/soul-client/springcloud-register";
         executorService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
     }
 
-    private void handler(final ServiceBean serviceBean) {
-        Class<?> clazz = serviceBean.getRef().getClass();
-        if (ClassUtils.isCglibProxyClass(clazz)) {
-            String superClassName = clazz.getGenericSuperclass().getTypeName();
-            try {
-                clazz = Class.forName(superClassName);
-            } catch (ClassNotFoundException e) {
-                log.error(String.format("class not found: %s", superClassName));
-                return;
+    @Override
+    public Object postProcessAfterInitialization(@NonNull final Object bean, @NonNull final String beanName) throws BeansException {
+        Controller controller = AnnotationUtils.findAnnotation(bean.getClass(), Controller.class);
+        RestController restController = AnnotationUtils.findAnnotation(bean.getClass(), RestController.class);
+        RequestMapping requestMapping = AnnotationUtils.findAnnotation(bean.getClass(), RequestMapping.class);
+        if (controller != null || restController != null || requestMapping != null) {
+            String contextPath = config.getContextPath();
+            //首先
+            String prePath = "";
+            SoulSpringCloudClient clazzAnnotation = AnnotationUtils.findAnnotation(bean.getClass(), SoulSpringCloudClient.class);
+            if (Objects.nonNull(clazzAnnotation)) {
+                if (clazzAnnotation.path().indexOf("*") > 1) {
+                    String finalPrePath = prePath;
+                    executorService.execute(() -> post(buildJsonParams(clazzAnnotation, contextPath, finalPrePath)));
+                    return bean;
+                }
+                prePath = clazzAnnotation.path();
+            }
+            final Method[] methods = ReflectionUtils.getUniqueDeclaredMethods(bean.getClass());
+            for (Method method : methods) {
+                SoulSpringCloudClient soulSpringCloudClient = AnnotationUtils.findAnnotation(method, SoulSpringCloudClient.class);
+                if (Objects.nonNull(soulSpringCloudClient)) {
+                    String finalPrePath = prePath;
+                    executorService.execute(() -> post(buildJsonParams(soulSpringCloudClient, contextPath, finalPrePath)));
+                }
             }
         }
-        final Method[] methods = ReflectionUtils.getUniqueDeclaredMethods(clazz);
-        for (Method method : methods) {
-            SoulDubboClient soulDubboClient = method.getAnnotation(SoulDubboClient.class);
-            if (Objects.nonNull(soulDubboClient)) {
-                post(buildJsonParams(serviceBean, soulDubboClient, method));
-            }
-        }
-    }
-
-    private String buildJsonParams(final ServiceBean serviceBean, final SoulDubboClient soulDubboClient, final Method method) {
-        String appName = dubboConfig.getAppName();
-        if (appName == null || "".equals(appName)) {
-            appName = serviceBean.getApplication().getName();
-        }
-        String path = dubboConfig.getContextPath() + soulDubboClient.path();
-        String desc = soulDubboClient.desc();
-        String serviceName = serviceBean.getInterface();
-        String configRuleName = soulDubboClient.ruleName();
-        String ruleName = ("".equals(configRuleName)) ? path : configRuleName;
-        String methodName = method.getName();
-        Class<?>[] parameterTypesClazz = method.getParameterTypes();
-        String parameterTypes = Arrays.stream(parameterTypesClazz).map(Class::getName)
-                .collect(Collectors.joining(","));
-        MetaDataDTO metaDataDTO = MetaDataDTO.builder()
-                .appName(appName)
-                .serviceName(serviceName)
-                .methodName(methodName)
-                .contextPath(dubboConfig.getContextPath())
-                .path(path)
-                .ruleName(ruleName)
-                .pathDesc(desc)
-                .parameterTypes(parameterTypes)
-                .rpcExt(buildRpcExt(serviceBean))
-                .rpcType("dubbo")
-                .enabled(soulDubboClient.enabled())
-                .build();
-        return OkHttpTools.getInstance().getGosn().toJson(metaDataDTO);
-
-    }
-
-    private String buildRpcExt(final ServiceBean serviceBean) {
-        MetaDataDTO.RpcExt build = MetaDataDTO.RpcExt.builder()
-                .group(StringUtils.isNotEmpty(serviceBean.getGroup()) ? serviceBean.getGroup() : "")
-                .version(StringUtils.isNotEmpty(serviceBean.getVersion()) ? serviceBean.getVersion() : "")
-                .loadbalance(StringUtils.isNotEmpty(serviceBean.getLoadbalance()) ? serviceBean.getLoadbalance() : Constants.DEFAULT_LOADBALANCE)
-                .retries(Objects.isNull(serviceBean.getRetries()) ? Constants.DEFAULT_RETRIES : serviceBean.getRetries())
-                .timeout(Objects.isNull(serviceBean.getTimeout()) ? Constants.DEFAULT_CONNECT_TIMEOUT : serviceBean.getTimeout())
-                .url("")
-                .build();
-        return OkHttpTools.getInstance().getGosn().toJson(build);
-
+        return bean;
     }
 
     private void post(final String json) {
         try {
             String result = OkHttpTools.getInstance().post(url, json);
             if (Objects.equals(result, "success")) {
-                log.info("dubbo client register success :{} ", json);
+                log.info("http client register success :{} ", json);
             } else {
-                log.error("dubbo client register error :{} ", json);
+                log.error("http client register error :{} ", json);
             }
         } catch (IOException e) {
             log.error("cannot register soul admin param :{}", url + ":" + json);
         }
     }
 
-    @Override
-    public void onApplicationEvent(final ContextRefreshedEvent contextRefreshedEvent) {
-        if (Objects.nonNull(contextRefreshedEvent.getApplicationContext().getParent())) {
-            return;
-        }
-        // Fix bug(https://github.com/dromara/soul/issues/415), upload dubbo metadata on ContextRefreshedEvent
-        Map<String, ServiceBean> serviceBean = contextRefreshedEvent.getApplicationContext().getBeansOfType(ServiceBean.class);
-        for (Map.Entry<String, ServiceBean> entry : serviceBean.entrySet()) {
-            executorService.execute(() -> handler(entry.getValue()));
-        }
+    private String buildJsonParams(final SoulSpringCloudClient soulSpringCloudClient, final String contextPath, final String prePath) {
+        String appName = env.getProperty("spring.application.name");
+        String path = contextPath + prePath + soulSpringCloudClient.path();
+        String desc = soulSpringCloudClient.desc();
+        String configRuleName = soulSpringCloudClient.ruleName();
+        String ruleName = ("".equals(configRuleName)) ? path : configRuleName;
+        SpringCloudRegisterDTO registerDTO = SpringCloudRegisterDTO.builder()
+                .context(contextPath)
+                .appName(appName)
+                .path(path)
+                .pathDesc(desc)
+                .rpcType(soulSpringCloudClient.rpcType())
+                .enabled(soulSpringCloudClient.enabled())
+                .ruleName(ruleName)
+                .build();
+        return OkHttpTools.getInstance().getGosn().toJson(registerDTO);
     }
 }
+
